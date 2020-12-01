c8ae13e5d824c9a8bb3043b37393a8748359fd4a
Jorge GC <72167604+JorgeGC123@users.noreply.github.com>
2020-12-01 17:13:11 +0100

    bootstrap

--- Model/functions.php
+++ Model/functions.php
@@ -2,7 +2,7 @@
 define('__ROOT__', dirname(dirname(__FILE__)));
 
 function get_header(){
-    require_once(__ROOT__.'/Views/header.php');
+    require_once(__ROOT__.'/View/header.php');
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <!DOCTYPE html>
+
 <head>
 	<title>Roguelike</title>
 
-	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<!-- Custom styles for this template -->
 	<link rel="stylesheet" href="css/styles.css">
@@ -13,14 +24,26 @@
 	<script src="js/asciidisplay.js"></script>
 	<script src="js/main.js"></script>
 
-	
+
 </head>
 <?php require_once('./Model/functions.php'); ?>
+
 <body>
 
 	<?php get_header(); ?>
-	<div ><div id='xd'></div></div>
-
+	<div class="container-fluid">
+		<div class="row" id="central">
+			<div class="col-sm">
+				Bloque con la información del jugador
+			</div>
+			<div class="col-sm">
+				<div id='xd'></div>
+			</div>
+			<div class="col-sm">
+				Bloque con la información del mapa y los enemigos
+			</div>
+		</div>
+	</div>
 </body>
 
 </html>
