baaf019b2f20cf795f8066afd061c6abfd672201
Jorge GC <72167604+JorgeGC123@users.noreply.github.com>
2020-12-01 11:30:24 +0100

    ai 0.3

--- js/actor.js
+++ js/actor.js
@@ -20,5 +20,50 @@ class actor {
             this.y + dir.y <= ROWS - 1 &&
             mapa.getPosition(this.y + dir.y, this.x + dir.x) == '.';
     }
-  
+    moveTo(mapa, dir) {
+        // check if this can move in the given direction
+        
+        if (!this.canGo(mapa, dir))
+            return false;
+    
+        // moves this to the new location
+        var newKey = (this.y + dir.y) + '_' + (this.x + dir.x);
+        // if the destination tile has an this in it 
+        if (actorMap[newKey] != null) {
+            console.log(this.name + ': TOMA HOSTIA CABRÓN CON NOMBRE ' + actorMap[newKey].name)
+            //decrement hitpoints of the this at the destination tile
+            var victim = actorMap[newKey];
+            victim.hp--;
+    
+            // if it's dead remove its reference 
+            if (victim.hp == 0) {
+                console.log(victim.name + ': ME MORÍ! NOOO :(')
+                actorMap[newKey] = null;
+                //actorList[actorList.indexOf(victim)] = null;
+                var index = actorList.indexOf(victim);
+                if (index > -1) {
+                    actorList.splice(index, 1);
+                }
+                if (victim != player) {
+                    livingEnemies--;
+                    if (livingEnemies == 0) {
+                        // victory message
+                        var victory = game.add.text(game.world.centerX, game.world.centerY, 'Victory!\nCtrl+r to restart', { fill: '#2e2', align: "center" });
+                        victory.anchor.setTo(0.5, 0.5);
+                    }
+                }
+            }
+        } else {
+            // remove reference to the this's old position
+            actorMap[this.y + '_' + this.x] = null;
+    
+            // update position
+            this.y += dir.y;
+            this.x += dir.x;
+    
+            // add reference to the this's new position
+            actorMap[this.y + '_' + this.x] = this;
+        }
+        return true;
+    }
 }
--- js/main.js
+++ js/main.js
@@ -47,19 +47,19 @@ function onKeyUp(event) {
     var acted = false;
     switch (event.keyCode) {
         case Phaser.Keyboard.LEFT:
-            acted = moveTo(player, { x: -1, y: 0 });
+            acted = player.moveTo(mapa, { x: -1, y: 0 });
             break;
 
         case Phaser.Keyboard.RIGHT:
-            acted = moveTo(player, { x: 1, y: 0 });
+            acted = player.moveTo(mapa, { x: 1, y: 0 });
             break;
 
         case Phaser.Keyboard.UP:
-            acted = moveTo(player, { x: 0, y: -1 });
+            acted = player.moveTo(mapa, { x: 0, y: -1 });
             break;
 
         case Phaser.Keyboard.DOWN:
-            acted = moveTo(player, { x: 0, y: 1 });
+            acted = player.moveTo(mapa, { x: 0, y: 1 });
             break;
     }
 
@@ -87,25 +87,25 @@ function aiAct(actor1) {
     if (Math.abs(dx) + Math.abs(dy) > 6)
         // try to walk in random directions until you succeed once
 
-        while (!moveTo(actor1, directions[Math.floor(Math.random() * directions.length)])) { };
+        while (!actor1.moveTo(mapa, directions[Math.floor(Math.random() * directions.length)])) { };
 
     // otherwise walk towards player
     if (Math.abs(dx) > Math.abs(dy)) {
         console.log(actor1.name + ': Te voy a matar hijode puta');
         if (dx < 0) {
             // left
-            moveTo(actor1, directions[0]);
+            actor1.moveTo(mapa,directions[0]);
         } else {
             // right
-            moveTo(actor1, directions[1]);
+            actor1.moveTo(mapa,directions[1]);
         }
     } else {
         if (dy < 0) {
             // up
-            moveTo(actor1, directions[2]);
+            actor1.moveTo(mapa, directions[2]);
         } else {
             // down
-            moveTo(actor1, directions[3]);
+            actor1.moveTo(mapa, directions[3]);
         }
     }
     if (player.hp < 1) {
@@ -115,52 +115,6 @@ function aiAct(actor1) {
     }
 }
 
-
-function moveTo(actor1, dir) {
-    // check if actor1 can move in the given direction
-    if (!actor1.canGo(mapa, dir))
-        return false;
-
-    // moves actor1 to the new location
-    var newKey = (actor1.y + dir.y) + '_' + (actor1.x + dir.x);
-    // if the destination tile has an actor1 in it 
-    if (actorMap[newKey] != null) {
-        console.log(actor1.name + ': TOMA HOSTIA CABRÓN CON NOMBRE ' + actorMap[newKey].name)
-        //decrement hitpoints of the actor1 at the destination tile
-        var victim = actorMap[newKey];
-        victim.hp--;
-
-        // if it's dead remove its reference 
-        if (victim.hp == 0) {
-            console.log(victim.name + ': ME MORÍ! NOOO :(')
-            actorMap[newKey] = null;
-            //actorList[actorList.indexOf(victim)] = null;
-            var index = actorList.indexOf(victim);
-            if (index > -1) {
-                actorList.splice(index, 1);
-            }
-            if (victim != player) {
-                livingEnemies--;
-                if (livingEnemies == 0) {
-                    // victory message
-                    var victory = game.add.text(game.world.centerX, game.world.centerY, 'Victory!\nCtrl+r to restart', { fill: '#2e2', align: "center" });
-                    victory.anchor.setTo(0.5, 0.5);
-                }
-            }
-        }
-    } else {
-        // remove reference to the actor1's old position
-        actorMap[actor1.y + '_' + actor1.x] = null;
-
-        // update position
-        actor1.y += dir.y;
-        actor1.x += dir.x;
-
-        // add reference to the actor1's new position
-        actorMap[actor1.y + '_' + actor1.x] = actor1;
-    }
-    return true;
-}
 
 
 
